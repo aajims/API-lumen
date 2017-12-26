de4566818f5ed41a6de199392d1c76ac98bb6cd9
imajinyun <imajinyun@gmail.com>
2017-12-26 22:05:20 +0800

    : ) Add helpers

---
+++ app/Http/Bar/Controllers/AuthenticateController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Bar\Controllers;
+
+/**
+ * AuthenticateController Class.
+ *
+ * @copyright Copyright (c) Xiaohe Software Foundation, Inc.
+ * @link      https://www.xiaohe.com/ Xiaohe(tm) Project
+ * @package   App\Http\Bar\Controllers
+ * @date      2017-12-26 15:50
+ * @author    majinyun <majinyun@xiaohe.com>
+ */
+class AuthenticateController
+{
+
+}

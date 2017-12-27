ea74f3f5393c6b7aaa6e535f55e6c812540df7b7
imajinyun <imajinyun@gmail.com>
2017-12-27 17:20:29 +0800

    : ) Update .gitignore

---
+++ app/Http/Auth/Controllers/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Auth\Controllers;
+
+/**
+ * DefaultController Class.
+ *
+ * @copyright Copyright (c) Xiaohe Software Foundation, Inc.
+ * @link      https://www.xiaohe.com/ Xiaohe(tm) Project
+ * @package   App\Http\Auth\Controllers
+ * @date      2017-12-27 16:52
+ * @author    majinyun <majinyun@xiaohe.com>
+ */
+class DefaultController
+{
+
+}

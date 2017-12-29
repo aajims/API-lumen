4a949634c6315978925001abf31da4e5604e33ee
imajinyun <imajinyun@gmail.com>
2017-12-29 10:19:23 +0800

    : ) Fixed an issue that the expression token of type is loosely compared to boolean value

--- app/Http/Auth/Controllers/AuthenticateController.php
+++ app/Http/Auth/Controllers/AuthenticateController.php
@@ -49,7 +49,7 @@ class AuthenticateController extends Controller
             $credentials = $request->only('email', 'password');
             $token = $this->jwtAuth->attempt($credentials);
 
-            if (! $token) {
+            if ($token === false) {
                 return self::successResponse(
                     [],
                     'The user not found.',
